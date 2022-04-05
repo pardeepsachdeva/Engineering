27af0c7461f295a35bacb85c11878bfd786bd757
Vikasupadhyay007 <vikasupadhyay983@gmail.com>
2022-04-05 15:23:44 +0530

    Update menu.py

--- menu.py
+++ menu.py
@@ -4,12 +4,23 @@ from tkinter import *
 root =Tk()
 root.geometry("300x200")
 
-my_menu = Menu(root)
+def demo():
+    pass
+
+# Menu 
+my_menu = Menu(root)            
 root.config(menu=my_menu)
 
 file_menu = Menu(my_menu)
 my_menu.add_cascade(label="File",menu=file_menu)
 
+
+eng_menu = Menu(my_menu)
+my_menu.add_cascade(label="Engineering",menu=eng_menu)
+#menu option
+eng_menu.add_cascade(label="Area",command=demo)
+eng_menu.add_separator()                                  #separate two menu option
+eng_menu.add_cascade(label="Volume",command=demo)
 
 
 root.mainloop()
